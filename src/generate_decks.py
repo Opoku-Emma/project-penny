22b3488fc474ff3755f79ebdb343b366fcf7dcfd
Opoku-Emma <eopoku@wm.edu>
2026-09-15 14:27:05 -0400

    Added module for data generation

---
+++ src/generate_decks.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from src.paths import PATH_DATA_RAW
+import src.generate_seed as g_seed
+
+
+class DeckGenerator:
+    """Utility class to create and save deck of cards"""
+
+    def __init__(self) -> None:
+        self.seed_logger = g_seed.SeedGenerator()
+        self.PATH_DECKS = PATH_DATA_RAW / "decks"
+        self.current_seed = self.seed_logger.seed
+        # this will be used to check if a deck has been
+        # generated. if they are the same, an error will pop up
+        self.previous_seed = self.current_seed
+
+    def make_decks(self, num_decks: int, num_cards: int) -> np.ndarray:
+        """Generate a deck of cards with size (num_decks, num_cards)"""
+
+        # set seed
+        self.current_seed = self.seed_logger.get_next_seed()
+        rng = np.random.default_rng(self.current_seed)
+
+        self.current_decks = rng.integers(low=0, high=2, size=(num_decks, num_cards))
+
+        return self.current_decks
+
+    def save_deck(self) -> None:
+        '''Store simulated deck as numpy arrays for later retrieval'''
+        if self.current_seed == self.previous_seed and (self.previous_seed != 0):
+            # print("Generate decks before saving!")
+            raise RuntimeError("Generate decks before saving!")
+
+        self.PATH_DECKS.mkdir(parents=True, exist_ok=True)
+        n_decks = self.current_decks.shape[0]
+        n_cards = self.current_decks.shape[1]
+
+        filename = (
+            self.PATH_DECKS / f"decks_{n_decks}x{n_cards}_seed_{self.current_seed}"
+        )
+        np.save(filename, self.current_decks)
+        print()
+        self.seed_logger.save_seed_info()
+        return
--- tests/test_deck_generator.py
+++ tests/test_deck_generator.py
@@ -1,8 +1,9 @@
 from src import generate_decks as generate_decks
 
+print('Making deck generator object')
 deck_obj = generate_decks.DeckGenerator()
-
+print('Making decks')
 c_decks = deck_obj.make_decks(2, 52)
-print(c_decks)
 
+print('Saving decks')
 deck_obj.save_deck()

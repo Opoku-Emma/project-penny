0c156e029eb3d9f5f951ae1bf21fdada56fd3435
Opoku-Emma <eopoku@wm.edu>
2026-09-16 22:42:13 -0400

    Updated tracking for simulation

--- tests/test_process_data.py
+++ tests/test_process_data.py
@@ -1,22 +1,7 @@
-from src.dataproc import read_raw_data, convert_numpy_str, make_player_pairs, simulate_game
-from src.paths import PATH_DATA_RAW_DECKS
-
-
-def test_process_data():
-    data = read_raw_data(PATH_DATA_RAW_DECKS)
-    return
-
-
-def test_convert_numpy_to_str():
-    data = read_raw_data(PATH_DATA_RAW_DECKS)
-    # converted_data = convert_numpy_str(data)
-    return
+from src.dataproc import make_player_pairs, simulate_game
 
 
 def test_simulate_game():
-    # data = read_raw_data(PATH_DATA_RAW_DECKS)
-    # print("Converting data")
-    # converted_data = convert_numpy_str(data)
     print("Simulating game")
     possible_combinations, card_combination = make_player_pairs(3)
     results = simulate_game(possible_combinations, card_combination)
@@ -25,5 +10,4 @@ def test_simulate_game():
 
 
 if __name__ == "__main__":
-    # data = test_process_data()
     test_simulate_game()

1d53906757ccc94714292ad3c7b5ce1a964c42af
mjserena <33012412+mjserena@users.noreply.github.com>
2026-09-18 12:17:15 -0400

    Update datavis.py

--- src/datavis.py
+++ src/datavis.py
@@ -25,7 +25,7 @@ def datavis(N="TBD"):
 
    # Need this for row and column labels
     color_patterns = np.load(paths.PATH_DATA_CLEAN / "possible_pairs.npy")
-    print(f"Using color patterns {color_patterns})
+    print(f"Using color patterns {color_patterns}")
 
     # Read in "Classic" Wins
     classic_wins_df = pd.DataFrame(
